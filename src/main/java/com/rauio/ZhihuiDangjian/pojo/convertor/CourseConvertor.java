66bf46bfe2f493dded286a34ec0ccb2704c08c72
Rauio <Molqaq101@outlook.com>
2025-11-14 23:03:58 +0800

    水一个警告

--- src/main/java/com/rauio/ZhihuiDangjian/pojo/convertor/CourseConvertor.java
+++ src/main/java/com/rauio/ZhihuiDangjian/pojo/convertor/CourseConvertor.java
@@ -4,8 +4,9 @@ import com.rauio.ZhihuiDangjian.pojo.Course;
 import com.rauio.ZhihuiDangjian.pojo.dto.CourseDto;
 import com.rauio.ZhihuiDangjian.pojo.vo.CourseVO;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CourseConvertor {
     Course      CourseDtoToCourse(CourseDto courseDto);
     CourseVO    CourseToCourseVO(Course course);
