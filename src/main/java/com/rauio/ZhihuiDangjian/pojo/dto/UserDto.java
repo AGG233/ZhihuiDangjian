36770361f1290a75824c4ed26d39e64a04e6e63e
Rauio <Molqaq101@outlook.com>
2025-11-30 21:21:14 +0800

    加个字段和文档

--- src/main/java/com/rauio/ZhihuiDangjian/pojo/dto/UserDto.java
+++ src/main/java/com/rauio/ZhihuiDangjian/pojo/dto/UserDto.java
@@ -1,34 +1,52 @@
 package com.rauio.ZhihuiDangjian.pojo.dto;
 
-import com.rauio.ZhihuiDangjian.pojo.User;
+import com.rauio.ZhihuiDangjian.annotation.validation.Sensitive;
 import com.rauio.ZhihuiDangjian.utils.Spec.UserStatus;
 import com.rauio.ZhihuiDangjian.utils.Spec.UserType;
-import com.rauio.ZhihuiDangjian.annotation.validation.Sensitive;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class UserDto {
-    private String  userId;
-    private String  username;
-    private String  realName;
-    private String  party_member_id;
-    private UserStatus party_status;
-    private String  branch_name;
-    private UserType user_type;
-    @Sensitive(type = Sensitive.SensitiveType.EMAIL)
-    private String  email;
-    @Sensitive(type = Sensitive.SensitiveType.PHONE)
-    private String  phone;
 
-    public UserDto(User user) {
-        this.userId = user.getId();
-        this.username = user.getUsername();
-        this.realName = user.getRealName();
-        this.party_member_id = String.valueOf(user.getPartyMemberId());
-        this.party_status = user.getPartyStatus();
-        this.branch_name = user.getBranchName();
-        this.user_type = user.getUserType();
-        this.email = user.getEmail();
-        this.phone = user.getPhone();
-    }
+    @Schema(description = "用户ID，留空即可")
+    private String  userId;
+
+    @Schema(description = "用户名")
+    private String  username;
+
+    @Schema(description = "真实姓名")
+    private String  realName;
+
+    @Schema(description = "密码")
+    private String  password;
+
+    @Schema(description = "党员ID")
+    private String  party_member_id;
+
+    @Schema(description = "党员状态")
+    private UserStatus party_status;
+
+    @Schema(description = "党支部名称")
+    private String  branch_name;
+
+    @Schema(description = "用户类型")
+    private UserType user_type;
+
+    @Schema(description = "学校ID")
+    private String  university_id;
+
+    @Schema(description = "入党时间")
+    private Date    joinPartyDate;
+
+    @Sensitive(type = Sensitive.SensitiveType.EMAIL)
+    @Schema(description = "邮箱")
+    private String  email;
+
+
+    @Sensitive(type = Sensitive.SensitiveType.PHONE)
+    @Schema(description = "手机号")
+    private String  phone;
 }
