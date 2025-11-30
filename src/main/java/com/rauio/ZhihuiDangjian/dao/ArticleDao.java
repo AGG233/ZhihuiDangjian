22faaa50cab54aef2503ed8d07fb48f61124654b
Rauio <Molqaq101@outlook.com>
2025-11-30 21:12:59 +0800

    完善增删查改

--- src/main/java/com/rauio/ZhihuiDangjian/dao/ArticleDao.java
+++ src/main/java/com/rauio/ZhihuiDangjian/dao/ArticleDao.java
@@ -1,7 +1,9 @@
 package com.rauio.ZhihuiDangjian.dao;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.rauio.ZhihuiDangjian.mapper.ArticleMapper;
 import com.rauio.ZhihuiDangjian.pojo.Article;
+import com.rauio.ZhihuiDangjian.pojo.CategoryArticle;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -11,11 +13,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ArticleDao {
     private final ArticleMapper articleMapper;
+    private final CategoryArticleDao categoryArticleDao;
 
     public Article get(String id) {
         return articleMapper.selectById(id);
     }
 
-    public List<Article> getAllArticlesOfCategory(String id) {
+    public List<CategoryArticle> getAllArticlesOfCategory(String id) {
+        return categoryArticleDao.getAllArticlesOfCategory(id);
     }
+    public List<Article> getPage(int pageNum, int pageSize) {
+        return articleMapper.selectPage(new Page<>(pageNum,pageSize),null).getRecords();
+    }
+    public Boolean create(Article article) {
+        return articleMapper.insert(article) > 0;
+    }
+    public Boolean update(Article article) {
+        return articleMapper.updateById(article) > 0;
+    }
+    public Boolean delete(String id) {
+        return articleMapper.deleteById(id) > 0;
+    }
+
 }
