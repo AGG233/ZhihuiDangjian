f5ec399d37e0e10cf353e7a75fce779470987589
Rauio <Molqaq101@outlook.com>
2025-11-30 21:22:39 +0800

    完善CRUD

--- src/main/java/com/rauio/ZhihuiDangjian/service/ArticleService.java
+++ src/main/java/com/rauio/ZhihuiDangjian/service/ArticleService.java
@@ -1,17 +1,17 @@
 package com.rauio.ZhihuiDangjian.service;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.rauio.ZhihuiDangjian.pojo.Article;
 import com.rauio.ZhihuiDangjian.pojo.CategoryArticle;
+import com.rauio.ZhihuiDangjian.pojo.dto.ArticleDto;
 
 import java.util.List;
 
 public interface ArticleService {
     Article get(String id);
     List<CategoryArticle> getAllArticlesOfCategory(String id);
-    Page<Article> getPage(int pageNum, int pageSize);
+    List<Article> getPage(int pageNum, int pageSize);
 
-    Boolean create(String courseId, String title, String content);
-    Boolean update(String id, String title, String content);
+    Boolean create(ArticleDto dto);
+    Boolean update(ArticleDto dto);
     Boolean delete(String id);
 }
