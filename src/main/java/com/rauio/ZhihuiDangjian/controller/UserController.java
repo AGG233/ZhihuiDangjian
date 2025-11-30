cdca819ac806ce9aa7c724c1bbfdd39a36f4e032
Rauio <Molqaq101@outlook.com>
2025-11-30 21:12:45 +0800

    弃用接口

--- src/main/java/com/rauio/ZhihuiDangjian/controller/UserController.java
+++ src/main/java/com/rauio/ZhihuiDangjian/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.rauio.ZhihuiDangjian.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.rauio.ZhihuiDangjian.aop.annotation.ResourceAccess;
 import com.rauio.ZhihuiDangjian.pojo.Course;
 import com.rauio.ZhihuiDangjian.pojo.User;
 import com.rauio.ZhihuiDangjian.pojo.response.ApiResponse;
@@ -30,9 +28,7 @@ public class UserController {
             summary = "获取用户信息",
             description = "通过ID获取用户信息")
     @GetMapping("/{id}")
-    public ResponseEntity<String> get(
-            @Parameter(description = "用户ID") @PathVariable String id
-    ) throws JsonProcessingException {
+    public ResponseEntity<String> get(@Parameter(description = "用户ID") @PathVariable String id){
         UserVO user = userService.getUserByID(id);
         return ApiResponse.buildResponse(user);
     }
@@ -42,28 +38,27 @@ public class UserController {
             description = "通过ID更新用户信息"
     )
     @PutMapping("/{id}")
-    @ResourceAccess(id = "#id")
     public ResponseEntity<String> update(
             @PathVariable String id,
             @RequestBody User user
-    ) throws JsonProcessingException {
+    ){
         Boolean result = userService.update(id,user);
         return ApiResponse.buildResponse(result);
     }
 
     @Operation(
-            summary = "删除用户",
+            summary = "删除用户（已经弃用）",
             description = "通过ID删除用户"
     )
     @DeleteMapping("/{id}")
-    @ResourceAccess(id = "#id")
-    public ResponseEntity<String> delete(@Parameter(description = "用户ID") @PathVariable String id) throws JsonProcessingException {
-        Boolean result = userService.delete(id);
-        return ApiResponse.buildResponse(result);
+    @Deprecated
+    public ResponseEntity<String> delete(@Parameter(description = "用户ID") @PathVariable String id){
+//        Boolean result = userService.delete(id);
+        return ApiResponse.buildResponse("404","接口已经弃用",null);
     }
 
     @GetMapping("/course/{id}")
-    public ResponseEntity<String> getAllCoursesOfUser(@PathVariable String id) throws JsonProcessingException {
+    public ResponseEntity<String> getAllCoursesOfUser(@PathVariable String id){
         List<Course> result = courseService.getAllCoursesOfUser(id);
         return ApiResponse.buildResponse(result);
     }
