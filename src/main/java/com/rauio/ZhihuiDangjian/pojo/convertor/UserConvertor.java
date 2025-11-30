623b4971eb9817653567af2f17ba7544d18dbc4e
Rauio <Molqaq101@outlook.com>
2025-11-30 21:14:12 +0800

    加几个转换

--- src/main/java/com/rauio/ZhihuiDangjian/pojo/convertor/UserConvertor.java
+++ src/main/java/com/rauio/ZhihuiDangjian/pojo/convertor/UserConvertor.java
@@ -6,11 +6,17 @@ import com.rauio.ZhihuiDangjian.pojo.vo.UserVO;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserConvertor {
     UserVO toVO(User user);
 
+    User toEntity(UserDto userDto);
     User toEntity(UserVO userVO);
 
     UserDto toDto(User user);
+
+    List<UserVO> toVO(List<User> userList);
+    List<User> toEntityList(List<UserDto> userList);
 }
