55665ce829b7831616f58338847eefa78176bf92
Rauio <Molqaq101@outlook.com>
2025-11-30 21:14:02 +0800

    增加预备表的Dao层

---
+++ src/main/java/com/rauio/ZhihuiDangjian/mapper/UserPermissionMapper.java
@@ -0,0 +1,10 @@
+package com.rauio.ZhihuiDangjian.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rauio.ZhihuiDangjian.pojo.UserPermission;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserPermissionMapper extends BaseMapper<UserPermission> {
+}
