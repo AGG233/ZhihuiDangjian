05a4644963ff9cd86878581e0177a3dcf099a9f6
Rauio <Molqaq101@outlook.com>
2025-12-01 16:27:57 +0800

    完善文档

--- src/main/java/com/rauio/ZhihuiDangjian/pojo/dto/CategoryDto.java
+++ src/main/java/com/rauio/ZhihuiDangjian/pojo/dto/CategoryDto.java
@@ -1,5 +1,6 @@
 package com.rauio.ZhihuiDangjian.pojo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,13 +8,25 @@ import java.util.List;
 
 @Data
 @Builder
+@Schema(description = "分类/目录请求体")
 public class CategoryDto {
-    private String  name;
-    private String  description;
+
+    @Schema(description = "分类层级，最大不超过三级")
     private final int level = 0;
+    @Schema(description = "分类名称")
+    private String  name;
+    @Schema(description = "分类描述")
+    private String  description;
+    @Schema(description = "分类父级ID")
     private String  parentId;
+
+    @Schema(description = "分类排序")
     private String  sortOrder;
+
+    @Schema(description = "分类状态")
     private Integer status;
+
+    @Schema(description = "子节点")
     private List<CategoryDto> childrenNode;
 
 }
