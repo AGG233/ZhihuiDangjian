2c314814fa7ec7e1233bb99249ad2a85c453b332
Rauio <Molqaq101@outlook.com>
2025-12-02 17:03:47 +0800

    用户考试接口

--- src/main/java/com/rauio/ZhihuiDangjian/controller/UserController.java
+++ src/main/java/com/rauio/ZhihuiDangjian/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.rauio.ZhihuiDangjian.controller;
 
+import com.rauio.ZhihuiDangjian.aop.annotation.PermissionAccess;
 import com.rauio.ZhihuiDangjian.pojo.Course;
 import com.rauio.ZhihuiDangjian.pojo.User;
+import com.rauio.ZhihuiDangjian.pojo.UserQuizAnswer;
 import com.rauio.ZhihuiDangjian.pojo.response.ApiResponse;
 import com.rauio.ZhihuiDangjian.pojo.vo.UserVO;
 import com.rauio.ZhihuiDangjian.service.CourseService;
+import com.rauio.ZhihuiDangjian.service.UserQuizAnswerService;
 import com.rauio.ZhihuiDangjian.service.UserService;
+import com.rauio.ZhihuiDangjian.utils.Spec.UserType;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,6 +26,7 @@ public class UserController {
 
     private final UserService userService;
     private final CourseService courseService;
+    private final UserQuizAnswerService userQuizAnswerService;
 
     @Operation(
             summary = "获取用户信息",
@@ -59,6 +64,61 @@ public class UserController {
     @GetMapping("/course/{id}")
     public ApiResponse<List<Course>> getAllCoursesOfUser(@PathVariable String id){
         List<Course> result = courseService.getAllCoursesOfUser(id);
+        return ApiResponse.ok(result);
+    }
+
+    /*
+    * 用户考试信息
+    * */
+    @GetMapping("/{id}/quiz")
+    public ApiResponse<List<UserQuizAnswer>> getAllQuizAnswerOfUser(@PathVariable String id){
+        List<UserQuizAnswer> result = userQuizAnswerService.selectByUserId(id);
+        return ApiResponse.ok(result);
+    }
+
+    @GetMapping("/{id}/quiz/{quizId}")
+    public ApiResponse<List<UserQuizAnswer>> getQuizAnswerOfUser(@PathVariable String id, @PathVariable String quizId){
+        List<UserQuizAnswer> result = userQuizAnswerService.selectByUserIdAndQuizId(id,quizId);
+        return ApiResponse.ok(result);
+    }
+
+    @GetMapping("/{id}/quiz/{quizId}")
+    public ApiResponse<List<UserQuizAnswer>> getQuizAnswerOfQuiz(@PathVariable String quizId){
+        List<UserQuizAnswer> result = userQuizAnswerService.selectByQuizId(quizId);
+        return ApiResponse.ok(result);
+    }
+
+    @GetMapping("/{id}/quiz/{quizId}/{optionId}")
+    public ApiResponse<UserQuizAnswer> getQuizAnswerOfOption(@PathVariable String optionId){
+        UserQuizAnswer result = userQuizAnswerService.selectByOptionId(optionId);
+        return ApiResponse.ok(result);
+    }
+
+    @PostMapping("/{id}/quiz/{quizId}/{optionId}")
+    public ApiResponse<Boolean> createQuizAnswer(@PathVariable String id, @PathVariable String quizId, @PathVariable String optionId){
+        UserQuizAnswer userQuizAnswer = UserQuizAnswer.builder().build();
+        userQuizAnswer.setUserId(id);
+        userQuizAnswer.setQuizId(quizId);
+        userQuizAnswer.setOptionId(optionId);
+        Boolean result = userQuizAnswerService.insert(userQuizAnswer);
+
+        return ApiResponse.ok(result);
+    }
+
+    @PutMapping("/{id}/quiz/{quizId}/{optionId}")
+    public ApiResponse<Boolean> updateQuizAnswer(@PathVariable String id, @PathVariable String quizId, @PathVariable String optionId){
+        UserQuizAnswer userQuizAnswer = UserQuizAnswer.builder().build();
+        userQuizAnswer.setUserId(id);
+        userQuizAnswer.setQuizId(quizId);
+        userQuizAnswer.setOptionId(optionId);
+        Boolean result = userQuizAnswerService.update(userQuizAnswer);
+        return ApiResponse.ok(result);
+    }
+
+    @DeleteMapping("/{id}/quiz/{quizId}/{optionId}")
+    @PermissionAccess(UserType.MANAGER)
+    public ApiResponse<Boolean> deleteQuizAnswer(@PathVariable String id, @PathVariable String quizId, @PathVariable String optionId){
+        Boolean result = userQuizAnswerService.delete(quizId);
         return ApiResponse.ok(result);
     }
 }
