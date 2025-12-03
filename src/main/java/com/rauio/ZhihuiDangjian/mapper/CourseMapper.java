e75e7c4d6abc887f1cc0843b336a9faae8a8c5a4
Rauio <Molqaq101@outlook.com>
2025-12-03 20:17:27 +0800

    全文检索类似的课程（CB)

--- src/main/java/com/rauio/ZhihuiDangjian/mapper/CourseMapper.java
+++ src/main/java/com/rauio/ZhihuiDangjian/mapper/CourseMapper.java
@@ -4,8 +4,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.yulichang.base.MPJBaseMapper;
 import com.rauio.ZhihuiDangjian.pojo.Course;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface CourseMapper extends BaseMapper<Course>, MPJBaseMapper<Course> {
 
+    @Select("select id,title,description from course WHERE MATCH(title,description) AGAINST (#{keyword} in BOOLEAN MODE)")
+    List<Course> getCourse(String keyword);
 }
