1d5b96e662fbffe20b2f0a32eeff5025a1e165bf
Rauio <Molqaq101@outlook.com>
2025-12-08 19:27:25 +0800

    [feature] 用户修改密码，是否属于学校

--- src/main/java/com/rauio/ZhihuiDangjian/service/UserService.java
+++ src/main/java/com/rauio/ZhihuiDangjian/service/UserService.java
@@ -1,6 +1,8 @@
 package com.rauio.ZhihuiDangjian.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.rauio.ZhihuiDangjian.pojo.User;
+import com.rauio.ZhihuiDangjian.pojo.dto.UserDto;
 import com.rauio.ZhihuiDangjian.pojo.vo.UserVO;
 
 public interface UserService {
@@ -16,8 +18,8 @@ public interface UserService {
     Boolean     update(Long id,User user);
     Boolean     delete(Long id);
     Boolean     register(User user);
-    Boolean     changePassword(Long id, String password);
-    Boolean     isUserBelongsToTheSchool(Long id, String schoolId);
+    Boolean     changePassword(String oldPassword, String newPassword);
+    Boolean     isUserBelongsSchool(Long id, Long schoolId);
 
 
     Page<User> getUser(UserDto userDto, int pageNum, int pageSize);
