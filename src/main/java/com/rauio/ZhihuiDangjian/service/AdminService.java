67e17a06ac74ec9d40496df36f88be626e5b9bf3
Rauio <Molqaq101@outlook.com>
2025-12-09 20:42:16 +0800

    [debug] 移除不必要的参数

--- src/main/java/com/rauio/ZhihuiDangjian/service/AdminService.java
+++ src/main/java/com/rauio/ZhihuiDangjian/service/AdminService.java
@@ -3,19 +3,15 @@ package com.rauio.ZhihuiDangjian.service;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.rauio.ZhihuiDangjian.pojo.User;
 import com.rauio.ZhihuiDangjian.pojo.dto.UserDto;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface AdminService {
 
-    String addUser(@RequestBody List<UserDto> user);
-    String deleteUser(@RequestBody List<String> idList);
-    String updateUser(@RequestBody List<UserDto> user);
-
-    String addSchoolAdmin(@RequestBody List<UserDto> user);
-    String updateSchoolAdmin(@RequestBody List<UserDto> user);
-    String deleteSchoolAdmin(@RequestBody List<String> idList);
+    // 系统管理员功能
+    Boolean addUser(List<UserDto> user);
+    Boolean deleteUser(List<String> idList);
+    Boolean updateUser(List<UserDto> user);
 
     Page<User> getUser(UserDto userDto, int pageNum, int pageSize);
 }
