0f3afe94ba51a85038fa68c3a0288c5e60effd7c
Rauio <Molqaq101@outlook.com>
2025-12-10 15:31:26 +0800

    [feature]

--- src/main/java/com/rauio/ZhihuiDangjian/mapper/UserLearningRecordMapper.java
+++ src/main/java/com/rauio/ZhihuiDangjian/mapper/UserLearningRecordMapper.java
@@ -2,9 +2,17 @@ package com.rauio.ZhihuiDangjian.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.rauio.ZhihuiDangjian.pojo.UserLearningRecord;
+import com.rauio.ZhihuiDangjian.pojo.dto.UserBehaviorDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface UserLearningRecordMapper extends BaseMapper<UserLearningRecord> {
 
+    @Select("SELECT user_id, chapter_id FROM user_learning_record " +
+            "UNION " +
+            "SELECT user_id, chapter_id FROM user_chapter_progress")
+    List<UserBehaviorDto> getAllUserBehaviors();
 }
