491991933a44d74fda042c83872c0214e0fa2c39
Rauio <Molqaq101@outlook.com>
2025-12-10 10:38:38 +0800

    [feature] api文档，数据校验

--- src/main/java/com/rauio/ZhihuiDangjian/pojo/request/LoginRequest.java
+++ src/main/java/com/rauio/ZhihuiDangjian/pojo/request/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.rauio.ZhihuiDangjian.pojo.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,15 +11,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Schema(description = "登录请求体")
 public class LoginRequest {
-    @Schema(description = "用户的手机号码，邮箱，身份证号码等等")
+
+    @Schema(description = "用户的手机号码，邮箱，身份证号码等等",example = "admin")
+    @NotBlank(message = "请填写用户名/手机号码/身份证号码")
     private String  passport;
 
-    @Schema(description = "登录密码")
+    @Schema(description = "登录密码",example = "123456")
+    @NotBlank(message = "密码不能为空")
     private String  password;
 
     @Schema(description = "验证码的uuid")
+    @NotBlank(message = "请填写验证码uuid，前往/auth/captcha获取")
     private String  captchaUUID;
 
     @Schema(description = "验证码")
+    @NotBlank(message = "请填写验证码，前往/auth/captcha获取")
     private String  captchaCode;
 }
