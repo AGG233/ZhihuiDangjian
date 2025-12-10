ab404547565c67fb9dd0b32a812e1010f9be94e2
Rauio <Molqaq101@outlook.com>
2025-12-10 10:41:18 +0800

    [feature] 重命名ApiResponse

--- src/main/java/com/rauio/ZhihuiDangjian/service/AuthService.java
+++ src/main/java/com/rauio/ZhihuiDangjian/service/AuthService.java
@@ -2,15 +2,17 @@ package com.rauio.ZhihuiDangjian.service;
 
 import com.rauio.ZhihuiDangjian.pojo.request.LoginRequest;
 import com.rauio.ZhihuiDangjian.pojo.request.RegisterRequest;
-import com.rauio.ZhihuiDangjian.pojo.response.ApiResponse;
 import com.rauio.ZhihuiDangjian.pojo.response.LoginResponse;
+import com.rauio.ZhihuiDangjian.pojo.response.Result;
+
+import java.util.Map;
 
 public interface AuthService {
 
     LoginResponse login(LoginRequest loginRequest);
-    ApiResponse register(RegisterRequest registerRequest);
+    Result register(RegisterRequest registerRequest);
 
-    ApiResponse refresh(String refreshToken, String accessToken);
+    Result<Map<String,String>> refresh(String refreshToken);
 
-    ApiResponse logout(String refreshToken);
+    Result logout(String refreshToken);
 }
