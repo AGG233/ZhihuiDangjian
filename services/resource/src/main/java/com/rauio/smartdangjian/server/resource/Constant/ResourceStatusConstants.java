b9892190c7ca7d3c4b1405d1b7eb4abcf6fe138e
rauioRua <16919577+rauiorua@user.noreply.gitee.com>
2026-04-08 19:54:36 +0800

    feat:完善接口文档

---
+++ services/resource/src/main/java/com/rauio/smartdangjian/server/resource/Constant/ResourceStatusConstants.java
@@ -0,0 +1,11 @@
+package com.rauio.smartdangjian.server.resource.Constant;
+
+public final class ResourceStatusConstants {
+
+    private ResourceStatusConstants() {
+    }
+
+    public static final int DISABLED = 0;
+    public static final int AVAILABLE = 1;
+    public static final int DELETED = 2;
+}
