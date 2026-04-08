9969053488aea414a2e77ed5491b8b52e6b787ad
rauioRua <16919577+rauiorua@user.noreply.gitee.com>
2026-04-08 20:45:14 +0800

    fix:实体类与DDL一致

---
+++ services/resource/src/main/java/com/rauio/smartdangjian/server/resource/Constant/ResourceTypeConstants.java
@@ -0,0 +1,10 @@
+package com.rauio.smartdangjian.server.resource.Constant;
+
+public final class ResourceTypeConstants {
+
+    private ResourceTypeConstants() {
+    }
+
+    public static final int IMAGE = 0;
+    public static final int VIDEO = 1;
+}
