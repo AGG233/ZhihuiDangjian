cae79c6a7bd497e3c52b8dd8149c4613544d9e9f
AGG233 <molqaq101@outlook.com>
2026-06-04 15:59:28 +0800

    fix(config): add missing @Validated to AuthProperties

--- services/auth/src/main/java/com/rauio/smartdangjian/server/auth/config/AuthProperties.java
+++ services/auth/src/main/java/com/rauio/smartdangjian/server/auth/config/AuthProperties.java
@@ -1,10 +1,12 @@
 package com.rauio.smartdangjian.server.auth.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import lombok.Data;
 
 @Data
+@Validated
 @ConfigurationProperties(prefix = "auth")
 public class AuthProperties {
 
