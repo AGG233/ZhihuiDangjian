bba63c4bd8d15e80ea6af9b6270698331f9153bb
AGG233 <molqaq101@outlook.com>
2026-06-04 14:37:33 +0800

    [planexec] dev-auto-login-config/Q-1 - 完成编码

--- services/auth/src/main/java/com/rauio/smartdangjian/server/auth/config/DevProperties.java
+++ services/auth/src/main/java/com/rauio/smartdangjian/server/auth/config/DevProperties.java
@@ -3,6 +3,7 @@ package com.rauio.smartdangjian.server.auth.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import lombok.Data;
 
@@ -13,6 +14,7 @@ import lombok.Data;
  */
 @Data
 @Component
+@Validated
 @Profile("dev")
 @ConfigurationProperties(prefix = "app.dev")
 public class DevProperties {
