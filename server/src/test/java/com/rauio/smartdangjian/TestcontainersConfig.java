7b1745e4c3c5a9bfe04693a90aad9114bd35041d
AGG233 <molqaq101@outlook.com>
2026-06-07 12:42:45 +0800

    test: update testcontainers imports

--- server/src/test/java/com/rauio/smartdangjian/TestcontainersConfig.java
+++ server/src/test/java/com/rauio/smartdangjian/TestcontainersConfig.java
@@ -3,8 +3,8 @@ package com.rauio.smartdangjian;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.mysql.MySQLContainer;
-import org.testcontainers.neo4j.Neo4jContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.Neo4jContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -12,17 +12,17 @@ public class TestcontainersConfig {
 
     @Bean
     @ServiceConnection
-    MySQLContainer mysqlContainer() {
+    MySQLContainer<?> mysqlContainer() {
         DockerImageName mysqlImage =
                 DockerImageName.parse("public.ecr.aws/docker/library/mysql:8.4").asCompatibleSubstituteFor("mysql");
-        return new MySQLContainer(mysqlImage);
+        return new MySQLContainer<>(mysqlImage);
     }
 
     @Bean
     @ServiceConnection
-    Neo4jContainer neo4jContainer() {
+    Neo4jContainer<?> neo4jContainer() {
         DockerImageName neo4jImage =
                 DockerImageName.parse("public.ecr.aws/docker/library/neo4j:5").asCompatibleSubstituteFor("neo4j");
-        return new Neo4jContainer(neo4jImage);
+        return new Neo4jContainer<>(neo4jImage);
     }
 }
