7cd7471aacc4b7771b91c46bcacb6b0367a8bea6
AGG233 <molqaq101@outlook.com>
2026-06-07 11:10:05 +0800

    ci: make integration containers resilient to Docker Hub timeouts

--- server/src/test/java/com/rauio/smartdangjian/TestcontainersConfig.java
+++ server/src/test/java/com/rauio/smartdangjian/TestcontainersConfig.java
@@ -3,21 +3,26 @@ package com.rauio.smartdangjian;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.neo4j.Neo4jContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfig {
 
     @Bean
     @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>("mysql:8.4");
+    MySQLContainer mysqlContainer() {
+        DockerImageName mysqlImage =
+                DockerImageName.parse("public.ecr.aws/docker/library/mysql:8.4").asCompatibleSubstituteFor("mysql");
+        return new MySQLContainer(mysqlImage);
     }
 
     @Bean
     @ServiceConnection
-    Neo4jContainer<?> neo4jContainer() {
-        return new Neo4jContainer<>("neo4j:5");
+    Neo4jContainer neo4jContainer() {
+        DockerImageName neo4jImage =
+                DockerImageName.parse("public.ecr.aws/docker/library/neo4j:5").asCompatibleSubstituteFor("neo4j");
+        return new Neo4jContainer(neo4jImage);
     }
 }
